1caed6486ae390da4da3a9aa63070934ef0099c7
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-05-25 17:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
-if(typeof(input1)!==Number || typeof(input2)!==Number || input1=="" ||input2=="")
+if(input1 === "" ||
+    input2 === "" ||
+    isNaN(input1) ||
+    isNaN(input2))
 {
 	alert("Invalid input. Please enter a valid number.")
 	
